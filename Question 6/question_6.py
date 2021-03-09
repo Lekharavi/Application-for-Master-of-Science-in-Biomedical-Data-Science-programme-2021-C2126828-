a8f8981c6ec1c5c3841d07a87eef059a316763b9
Lekharavi <80312868+Lekharavi@users.noreply.github.com>
2021-03-09 22:46:59 +0800

    Update question_6.py

--- Question 6/question_6.py
+++ Question 6/question_6.py
@@ -6,12 +6,15 @@ Created on Tue Mar  9 16:45:15 2021
 """
 from shapely.geometry import Point, Polygon
 
-# Create Point objects
-p1 = Point(1,1)
-p2 = Point(1,5)
-
 # Create a Polygon
 coords = [(1,1), (1,5),(10,1), (10,5)]
 poly = Polygon(coords)
 
-#input the pompting questions
+points = input("Enter the points with space between the x and y coordinate: ").split()
+if len(points) != 2:
+    break
+point = Point (float(points[0]), float(points[1]))
+if point.within(poly):
+    print("Inside")
+else:
+    print("Outside")
